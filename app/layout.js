eaa5cde6f4c08d06809633f001f7f5bc7154dbc7
pptrgi <pptrgitonga@gmail.com>
2023-12-07 13:03:10 +0300

    Add Open graph and Twitter Card meta data

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,32 @@ import "./globals.css";
 import RootLayout from "../components/RootLayout";
 
 export const metadata = {
-  title: "p.Gitonga",
-  description: "A personal website",
+  title: "p.Gitonga | Personal Website",
+  description: "A personal portfolio website for Peter Gitonga",
+  authors: [{ name: "Peter Gitonga" }],
+  creator: "Peter Gitonga",
+  keywords: ["Portfolio", "Next.js", "Fullstack Developer", "Peter Gitonga"],
+  openGraph: {
+    title: "p.Gitonga | Personal Website",
+    description: "A personal portfolio website for Peter Gitonga",
+    url: "https://pgitonga.vercel.app",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dxw5yqr3s/image/upload/v1701943079/Open%20Graph/next_portfolio_kjrppb.png",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "p.Gitonga | Personal Website",
+    description: "A personal portfolio website for Peter Gitonga",
+    creator: "@ptrgitonga",
+    author: "@ptrgitonga",
+    images: [
+      "https://res.cloudinary.com/dxw5yqr3s/image/upload/v1701943079/Open%20Graph/next_portfolio_kjrppb.png",
+    ],
+  },
 };
 
 export default function Layout({ children }) {
