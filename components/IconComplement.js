3142e9e2083142db920551094bf6148145bb00b7
pptrgi <pptrgitonga@gmail.com>
2024-01-10 12:38:11 +0300

    Add a reusable component for complement icons

---
+++ components/IconComplement.js
@@ -0,0 +1,9 @@
+const IconComplement = ({ icon }) => {
+  return (
+    <span className="p-[4px] text-h3 rounded-full bg-titleColor text-bodyColor">
+      {icon}
+    </span>
+  );
+};
+
+export default IconComplement;
