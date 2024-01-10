cb1a8785df9e3ef78a873581338ccaa464b04002
pptrgi <pptrgitonga@gmail.com>
2024-01-10 12:42:06 +0300

    Style home header link

--- components/Header.js
+++ components/Header.js
@@ -18,20 +18,18 @@ const Header = () => {
             </Link>
             <div className="hidden justify-between items-center w-8/12 md800:flex lg1120:w-1/2">
               <div className="flex items-center gap-x-[1.5rem] md800:flex lg1023:gap-x-[2rem]">
-                <Link href={"/"} className="relative group">
-                  <span className="tracking-wide text-titleColor group-hover:text-titleColor/40">
-                    Home
-                  </span>
-                  <span className="block absolute bottom-0 left-0 w-[0.9rem] h-[0.125rem] bg-titleColor/60 group-hover:hidden"></span>
+                <Link href={"/"} className="relative">
+                  <span className="tracking-wide text-titleColor">Home</span>
+                  <span className="block absolute bottom-0 left-0 w-[0.9rem] h-[0.125rem] bg-titleColor/60"></span>
                 </Link>
                 <Link href={"/#about"} className="relative group">
-                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor">
+                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor trans_300">
                     About
                   </span>
                   <span className="hidden absolute bottom-0 left-0 w-[0.9rem] h-[0.125rem] bg-titleColor/60 group-hover:block"></span>
                 </Link>
                 <Link href={"/#projects"} className="relative group">
-                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor">
+                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor trans_300">
                     Projects
                   </span>
                   <span className="hidden absolute bottom-0 left-0 w-[0.9rem] h-[0.125rem] bg-titleColor/60 group-hover:block"></span>
@@ -39,7 +37,7 @@ const Header = () => {
               </div>
               <div className="flex items-center gap-x-[1.5rem] lg1023:gap-x-[2rem]">
                 <Link href={"/#cta"} className="relative group">
-                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor">
+                  <span className="tracking-wide text-titleColor/40 group-hover:text-titleColor trans_300">
                     Start a Project
                   </span>
                   <span className="hidden absolute bottom-0 left-0 w-[0.9rem] h-[0.125rem] bg-titleColor/60 group-hover:block"></span>
