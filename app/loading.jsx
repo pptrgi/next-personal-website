0c8bd77d55917fada009af502fdf4dc0d3a9d84b
pptrgi <pptrgitonga@gmail.com>
2024-01-24 17:07:37 +0300

    Add 404 and loading pages

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="h-screen w-full">
+      <div className="flex_center h-full w-full">
+        <p className="text-textColor/90">Just a sec ...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
