cabc2938e8f5b5bfe204abbcf3bdd5d753365c64
pptrgi <pptrgitonga@gmail.com>
2024-01-27 15:26:23 +0300

    Adjust spacing

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
     <div className="h-screen w-full">
       <div className="flex_center h-full w-full">
         <div className="flex_col gap-[2rem]">
-          <div className="flex_col gap-[0.75rem] items-center">
+          <div className="flex_col gap-[0.5rem] items-center">
             <h1 className="text-[4rem] leading-none">404</h1>
             <h3 className="text-h2Font leading-none">Page not found</h3>
           </div>
